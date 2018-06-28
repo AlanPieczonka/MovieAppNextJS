1e996f6f8fd0f8d80080d437419cdc6a63684662
Alan Pieczonka <alanpieczonka@gmail.com>
2018-06-28 09:51:32 +0200

    Configure Bulma and SCSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,21 @@
-export default () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-)
+import React from 'react';
+import style from '../styles/style.scss';
+
+let coe = "sdf";
+
+const Index = () => (
+	<div>
+		<style dangerouslySetInnerHTML={{ __html: style }} />
+
+		<div className="container">
+			<div className="columns">
+				<div className="column">First column</div>
+				<div className="column">Second column</div>
+				<div className="column">Third column</div>
+				<div className="column">Fourth column</div>
+			</div>
+		</div>
+	</div>
+);
+
+export default Index;
