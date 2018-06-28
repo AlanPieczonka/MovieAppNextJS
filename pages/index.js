466ee4bcfa9ac7886dd19e3c990adc61052595c6
Alan Pieczonka <alanpieczonka@gmail.com>
2018-06-28 23:39:06 +0200

    Fetch all movies as initialProps of the index.js page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
 import style from '../styles/style.scss';
 import Header from '../components/Header';
-import Main from '../components/Main';
+// import Main from '../components/Main';
+import API_KEY from '../api_key';
 
 const Index = () => (
 	<div>
 		<style dangerouslySetInnerHTML={{ __html: style }} />
 		<Header />
-		<Main />
+		{/* <Main movies={movies} /> */}
 	</div>
 );
+
+Index.getInitialProps = async () => {
+	const res = await fetch(
+		`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+	);
+	const { results } = await res.json();
+	return { movies: results };
+};
 
 export default Index;
