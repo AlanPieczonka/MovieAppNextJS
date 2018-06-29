597be989b21369315537c5e4e1eb1af47957aabd
Alan Pieczonka <alanpieczonka@gmail.com>
2018-06-29 11:12:36 +0200

    Display movies' genres in the Main component

--- components/Main.js
+++ components/Main.js
@@ -1,14 +1,19 @@
+import Link from 'next/link';
 import PropTypes from 'prop-types';
-import chunk from './../utils/chunk';
+import chunk from '../utils/chunk';
+import getGenres from '../utils/getGenres';
 
-const Main = ({ movies }) => {
+const Main = ({ movies, genres }) => {
 	const moviesList = chunk(movies, 4).map((section, i) => {
 		return (
 			<div className="columns" key={i}>
-				{section.map(({ title, poster_path, id }) => (
-					<div className="column" key={id}>
-						<img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={`Link to ${title}`} />
-						<div>{title}</div>
+				{section.map(({ title, poster_path, id, genre_ids }) => (
+					<div className="column movie-cell" key={id}>
+						<Link as={`/movies/${id}`} href={`/movie?id=${id}`}>
+							<img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt={`Link to ${title}`} />
+						</Link>
+						<div className="bold">{title}</div>
+						<div>{getGenres(genres, genre_ids).map((genre, i) => <p key={i}>{genre}</p>)}</div>
 					</div>
 				))}
 			</div>
@@ -23,6 +28,7 @@ const Main = ({ movies }) => {
 
 Main.propTypes = {
 	movies: PropTypes.array.isRequired,
+	genres: PropTypes.array.isRequired
 };
 
 export default Main;
--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,11 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import API_KEY from '../api_key';
 
-const Index = ({ movies }) => (
+const Index = ({ movies, genres }) => (
 	<div>
 		<style dangerouslySetInnerHTML={{ __html: style }} />
 		<Header />
-		<Main movies={movies} />
+		<Main movies={movies} genres={genres} />
 	</div>
 );
 
@@ -17,7 +17,16 @@ Index.getInitialProps = async () => {
 		`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
 	);
 	const { results } = await res.json();
-	return { movies: results };
+
+	const genresList = await fetch(
+		'https://api.themoviedb.org/3/genre/movie/list?api_key=25e0de523619bc43f6c36fe4fc6d97ed&language=en-US'
+	);
+	const { genres } = await genresList.json();
+
+	return {
+		movies: results,
+		genres
+	};
 };
 
 export default Index;
