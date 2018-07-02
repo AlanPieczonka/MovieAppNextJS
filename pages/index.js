d1335004d1091667f41698483e28006d70193056
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-02 16:51:14 +0200

    Rewrite index.js page to a class instead of functional component

--- pages/index.js
+++ pages/index.js
@@ -5,34 +5,39 @@ import Header from '../components/Header';
 import Main from '../components/Main';
 import API_KEY from '../api_key';
 
-const Index = ({ movies, genres }) => (
-	<div>
-		<style dangerouslySetInnerHTML={{ __html: style }} />
-		<Header />
-		<Main movies={movies} genres={genres} />
-	</div>
-);
+class Index extends React.Component {
+	static async getInitialProps() {
+		const res = await fetch(
+			`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
+		);
+		const { results } = await res.json();
 
-Index.getInitialProps = async () => {
-	const res = await fetch(
-		`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
-	);
-	const { results } = await res.json();
+		const genresList = await fetch(
+			'https://api.themoviedb.org/3/genre/movie/list?api_key=25e0de523619bc43f6c36fe4fc6d97ed&language=en-US'
+		);
+		const { genres } = await genresList.json();
 
-	const genresList = await fetch(
-		'https://api.themoviedb.org/3/genre/movie/list?api_key=25e0de523619bc43f6c36fe4fc6d97ed&language=en-US'
-	);
-	const { genres } = await genresList.json();
+		return {
+			movies: results,
+			genres
+		};
+	}
 
-	return {
-		movies: results,
-		genres
+	static propTypes = {
+		movies: PropTypes.array.isRequired,
+		genres: PropTypes.array.isRequired
 	};
-};
 
-Index.propTypes = {
-	movies: PropTypes.array.isRequired,
-	genres: PropTypes.array.isRequired,
+	render() {
+		const { movies, genres } = this.props;
+		return (
+			<div>
+				<style dangerouslySetInnerHTML={{ __html: style }} />
+				<Header />
+				<Main movies={movies} genres={genres} />
+			</div>
+		);
+	}
 }
 
 export default Index;
