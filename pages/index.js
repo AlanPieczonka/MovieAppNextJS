9ceca63225db8d33ac89bc6661047a5e39838402
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-05 16:20:07 +0200

    Remove movies from initialProps of the /index page

--- pages/index.js
+++ pages/index.js
@@ -3,39 +3,27 @@ import PropTypes from 'prop-types';
 import style from '../styles/style.scss';
 import Header from '../components/Header';
 import MoviesList from '../components/MovieList';
-import { getAllMovies, getAllGenres } from '../services/api';
+import { getAllGenres } from '../services/api';
 import { searchStore, appStore } from '../store';
 
 @observer
 class Index extends React.Component {
 	static async getInitialProps() {
-		const movies = await getAllMovies();
-		const genres = await getAllGenres();
-		// const movies = appStore.movies;
-
 		return {
-			movies,
-			genres
+			genres: await getAllGenres(),
 		};
 	}
 
 	static propTypes = {
-		movies: PropTypes.array.isRequired,
 		genres: PropTypes.array.isRequired
 	};
 
 	render() {
-		const { movies, genres } = this.props;
+		const { genres } = this.props;
 		return (
 			<div>
 				<style dangerouslySetInnerHTML={{ __html: style }} />
 				<Header searchStore={searchStore} />
-				<div className="text-center">
-					{'searchTerm from main store: ' + appStore.searchTermFromMainStore}
-				</div>
-				<div className="text-center">
-					{'requestStatus: ' + appStore.requestStatus}
-				</div>
 				<MoviesList requestStatus={appStore.requestStatus} movies={appStore.movies} genres={genres} />
 			</div>
 		);
