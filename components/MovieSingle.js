08c8006bb6819e2f96adbf875ae912114a298b1c
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-06 14:10:29 +0200

    Style MovieSingle component and add PropTypes

--- components/MovieSingle.js
+++ components/MovieSingle.js
@@ -1,35 +1,66 @@
 import Link from 'next/link';
+import PropTypes from 'prop-types';
 
 // todo: add propTypes
 class MovieSingle extends React.Component {
+	static propTypes = {
+		movie: PropTypes.shape({
+			title: PropTypes.string.isRequired,
+			release_date: PropTypes.string.isRequired,
+			runtime: PropTypes.number.isRequired,
+			vote_average: PropTypes.number.isRequired,
+			overview: PropTypes.string.isRequired,
+			genres: PropTypes.array.isRequired,
+			poster_path: PropTypes.string.isRequired
+		})
+	};
 	render() {
 		const { title, release_date, runtime, vote_average, overview, genres, poster_path } = this.props.movie;
 		return (
 			<React.Fragment>
-				<div className="container">
-					<div className="text-center padding--top25">
-						<Link href="/">
-							<button className="button">Go back to index page</button>
-						</Link>
-					</div>
-					<div className="columns">
-						<div className="column text-center">
-							<h1 className="title is-1">{title}</h1>
+				<div className="hero-body">
+					<div className="container has-text-centered">
+						<h1 className="title">{title}</h1>
+						<div className="columns">
+							<div className="column">
+								<img
+									src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+									className="image image-center"
+								/>
+							</div>
+							<div className="column">
+								<section>
+									<p className="title is-3">Runtime</p>
+									<p className="subtitle is-5">{runtime}</p>
+								</section>
+								<section>
+									<p className="title is-3">Release date</p>
+									<p className="subtitle is-5">{release_date}</p>
+								</section>
+								<section>
+									<p className="title is-3">Average vote</p>
+									<p className="subtitle is-5">{vote_average}</p>
+								<progress className="progress is-info" value={vote_average} max="10">
+									{vote_average}
+								</progress>
+								</section>
+								<section>
+								<p className="title is-3">Genres</p>
+								<p className="subtitle is-5">
+									{genres.map((genre) => (
+										<React.Fragment key={genre.id}>{genre.name} </React.Fragment>
+									))}
+								</p>
+								</section>
+								<p>{overview}</p>
+								<div className="text-center">
+									<Link href="/">
+										<button className="button margin--top25">Go back to the main page</button>
+									</Link>
+								</div>
+							</div>
 						</div>
-						<div className="column">
-							<h1 className="title is-1">NOT STYLED YET</h1>
-							<div>{release_date}</div>
-							<div>{runtime}</div>
-							<div>{vote_average}</div>
-							<div>{overview}</div>
-							<div>{genres.map((genre) => <p key={genre.name}>{genre.name}</p>)}</div>
-						</div>
 					</div>
-				</div>
-				<div className="column">
-					<figure className="image is-4by5">
-						<img src={`https://image.tmdb.org/t/p/w500/${poster_path}`} alt="Poster of the movie" />
-					</figure>
 				</div>
 			</React.Fragment>
 		);
