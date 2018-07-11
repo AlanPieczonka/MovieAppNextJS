34268e5da45bfdb135c4d02b4e575458b15a397a
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-11 14:07:11 +0200

    Improve properties readability in MovieSingle's render method

--- components/MovieSingle.js
+++ components/MovieSingle.js
@@ -15,57 +15,49 @@ class MovieSingle extends React.Component {
     })
   }
   render () {
-    const {
-      title,
-      release_date,
-      runtime,
-      vote_average,
-      overview,
-      genres,
-      poster_path
-    } = this.props.movie
+    const { movie } = this.props
     return (
-      <>'       '<div className='hero-body padding--top0 height100'>
+      <>'        \' '<div className='hero-body padding--top0 height100'>
         <div className='container has-text-centered'>
-          <h1 className='title'>{title}</h1>
+          <h1 className='title'>{movie.title}</h1>
           <div className='columns'>
             <div className='column'>
               <img
-                src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 className='image image-center'
               />
             </div>
             <div className='column'>
               <section>
                 <p className='title is-3'>Runtime</p>
-                <p className='subtitle is-5'>{runtime}m</p>
+                <p className='subtitle is-5'>{movie.runtime}m</p>
               </section>
               <section>
                 <p className='title is-3'>Release date</p>
-                <p className='subtitle is-5'>{release_date}</p>
+                <p className='subtitle is-5'>{movie.release_date}</p>
               </section>
               <section>
                 <p className='title is-3'>Average vote</p>
                 <p className='subtitle is-5' style={{ marginBottom: 5 }}>
-                  {vote_average}
+                  {movie.vote_average}
                 </p>
                 <progress
                   className='progress is-info'
-                  value={vote_average}
+                  value={movie.vote_average}
                   max='10'
                 >
-                  {vote_average}
+                  {movie.vote_average}
                 </progress>
               </section>
               <section style={{ marginBottom: 5 }}>
                 <p className='title is-3'>Genres</p>
                 <p className='subtitle is-5'>
-                  {genres.map(genre => (
+                  {movie.genres.map(genre => (
                     <Fragment key={genre.id}>{genre.name}&nbsp;</Fragment>
                   ))}
                 </p>
               </section>
-              <article>{overview}</article>
+              <article>{movie.overview}</article>
               <div className='text-center'>
                 <Link href='/'>
                   <button className='button margin--top25'>
@@ -76,7 +68,8 @@ class MovieSingle extends React.Component {
             </div>
           </div>
         </div>
-      </div>'     '</>
+      </div>' '
+      </>
     )
   }
 }
