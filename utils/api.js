0b58de0df324dcd0f5052c2a6b6eba0e49379cb1
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-11 11:59:00 +0200

    Change the way of exporting utils/api functions

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import fetch from 'isomorphic-unfetch'
 import API_KEY from '../api_key'
 
-export const getAllMovies = async () => {
+async function getAllMovies () {
   const res = await fetch(
     `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
   )
@@ -10,7 +10,7 @@ export const getAllMovies = async () => {
   return results
 }
 
-export const getSingleMovie = async id => {
+async function getSingleMovie (id) {
   const res = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}&language=en-US`
   )
@@ -19,7 +19,7 @@ export const getSingleMovie = async id => {
   return movie
 }
 
-export const searchMovie = async query => {
+async function searchMovie (query) {
   const res = await fetch(
     `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`
   )
@@ -28,7 +28,7 @@ export const searchMovie = async query => {
   return results
 }
 
-export const getAllGenres = async () => {
+async function getAllGenres () {
   const genresList = await fetch(
     'https://api.themoviedb.org/3/genre/movie/list?api_key=25e0de523619bc43f6c36fe4fc6d97ed&language=en-US'
   )
@@ -36,3 +36,5 @@ export const getAllGenres = async () => {
 
   return genres
 }
+
+export { getAllMovies, getSingleMovie, searchMovie, getAllGenres }
