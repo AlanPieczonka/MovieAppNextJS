9fc8af4d1b064af428ce2fef3f86220dfd9f36e5
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-17 19:21:39 +0200

    Simplify index page

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,16 @@
 import { Fragment } from 'react'
-import { observer, Provider } from 'mobx-react'
-import PropTypes from 'prop-types'
 import Header from '../components/Header'
 import MoviesList from '../components/MovieList'
-import { getAllGenres } from '../utils/api'
-import { searchStore, appStore } from '../store'
 
 import '../styles/style.scss'
 
-@observer
 class Index extends React.Component {
-  static async getInitialProps () {
-    const genres = await getAllGenres()
-    return { genres }
-  }
-
-  static propTypes = {
-    genres: PropTypes.array.isRequired
-  }
-
   render () {
-    const { genres } = this.props
     return (
-      <div>
-        <Provider searchStore={searchStore} appStore={appStore}>
-          <Fragment>
-            <Header />
-            <MoviesList genres={genres} />
-          </Fragment>
-        </Provider>
-      </div>
+      <Fragment>
+        <Header />
+        <MoviesList />
+      </Fragment>
     )
   }
 }
