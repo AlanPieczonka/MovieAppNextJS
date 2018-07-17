85cd4aa566a7c4ee0af3cbffe1b3da96fd346f11
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-17 15:17:11 +0200

    Small fix in utils/api.js

--- utils/api.js
+++ utils/api.js
@@ -30,7 +30,7 @@ async function searchMovie (query) {
 
 async function getAllGenres () {
   const genresList = await fetch(
-    'https://api.themoviedb.org/3/genre/movie/list?api_key=25e0de523619bc43f6c36fe4fc6d97ed&language=en-US'
+    `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`
   )
   const { genres } = await genresList.json()
 
