9bd1f5a98eb677f3eecb43c59fbc0467c4b14249
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-17 12:25:36 +0200

    Change <> to normal <Fragment> tag in MovieSingle component

--- components/MovieSingle.js
+++ components/MovieSingle.js
@@ -17,59 +17,60 @@ class MovieSingle extends React.Component {
   render () {
     const { movie } = this.props
     return (
-      <>'        \' '<div className='hero-body padding--top0 height100'>
-        <div className='container has-text-centered'>
-          <h1 className='title'>{movie.title}</h1>
-          <div className='columns'>
-            <div className='column'>
-              <img
-                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                className='image image-center'
-              />
-            </div>
-            <div className='column'>
-              <section>
-                <p className='title is-3'>Runtime</p>
-                <p className='subtitle is-5'>{movie.runtime}m</p>
-              </section>
-              <section>
-                <p className='title is-3'>Release date</p>
-                <p className='subtitle is-5'>{movie.release_date}</p>
-              </section>
-              <section>
-                <p className='title is-3'>Average vote</p>
-                <p className='subtitle is-5' style={{ marginBottom: 5 }}>
-                  {movie.vote_average}
-                </p>
-                <progress
-                  className='progress is-info'
-                  value={movie.vote_average}
-                  max='10'
-                >
-                  {movie.vote_average}
-                </progress>
-              </section>
-              <section style={{ marginBottom: 5 }}>
-                <p className='title is-3'>Genres</p>
-                <p className='subtitle is-5'>
-                  {movie.genres.map(genre => (
-                    <Fragment key={genre.id}>{genre.name}&nbsp;</Fragment>
-                  ))}
-                </p>
-              </section>
-              <article>{movie.overview}</article>
-              <div className='text-center'>
-                <Link href='/'>
-                  <button className='button margin--top25'>
+      <Fragment>
+        <div className='hero-body padding--top0 height100'>
+          <div className='container has-text-centered'>
+            <h1 className='title'>{movie.title}</h1>
+            <div className='columns'>
+              <div className='column'>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  className='image image-center'
+                />
+              </div>
+              <div className='column'>
+                <section>
+                  <p className='title is-3'>Runtime</p>
+                  <p className='subtitle is-5'>{movie.runtime}m</p>
+                </section>
+                <section>
+                  <p className='title is-3'>Release date</p>
+                  <p className='subtitle is-5'>{movie.release_date}</p>
+                </section>
+                <section>
+                  <p className='title is-3'>Average vote</p>
+                  <p className='subtitle is-5' style={{ marginBottom: 5 }}>
+                    {movie.vote_average}
+                  </p>
+                  <progress
+                    className='progress is-info'
+                    value={movie.vote_average}
+                    max='10'
+                  >
+                    {movie.vote_average}
+                  </progress>
+                </section>
+                <section style={{ marginBottom: 5 }}>
+                  <p className='title is-3'>Genres</p>
+                  <p className='subtitle is-5'>
+                    {movie.genres.map(genre => (
+                      <Fragment key={genre.id}>{genre.name}&nbsp;</Fragment>
+                    ))}
+                  </p>
+                </section>
+                <article>{movie.overview}</article>
+                <div className='text-center'>
+                  <Link href='/'>
+                    <button className='button margin--top25'>
                       Go back to the main page
-                  </button>
-                </Link>
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>' '
-      </>
+      </Fragment>
     )
   }
 }
