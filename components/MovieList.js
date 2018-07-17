87420bf9d6603d592cc683fb9788df7bcadad4ba
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-17 19:23:41 +0200

    Adjust MovieList to refactored store

--- components/MovieList.js
+++ components/MovieList.js
@@ -4,62 +4,61 @@ import PropTypes from 'prop-types'
 import getGenres from '../utils/getGenres'
 import _ from 'lodash/array'
 
-@inject('appStore')
+@inject('store')
 @observer
 class MoviesList extends React.Component {
   static propTypes = {
-    appStore: PropTypes.shape({
+    store: PropTypes.shape({
       movies: PropTypes.array.isRequired,
+      genres: PropTypes.array.isRequired,
       requestStatus: PropTypes.string.isRequired
-    }),
-    genres: PropTypes.array.isRequired
+    })
   }
-
   render () {
-    const { appStore: { movies, requestStatus }, genres } = this.props
     let moviesList
-    if (movies.length === 0 && requestStatus === 'done') {
-      moviesList = (
-        <h1 className='text-center height100'>
-          Unfortunately, we cannot find this movie!
-        </h1>
-      )
-    } else {
-      moviesList = _.chunk(movies, 4).map((section, i) => {
+    if (this.props.store.movies.length > 0) {
+      moviesList = _.chunk(this.props.store.movies, 4).map((section, i) => {
         return (
           <div className='columns columns--padding' key={i}>
-            {section.map(({ title, poster_path, id, genre_ids }) => (
-              <div className='column movie-cell' key={id}>
+            {section.map(movie => (
+              <div className='column movie-cell' key={movie.id}>
                 <Link
                   prefetch
-                  as={`movie/${id}`}
-                  href={{ pathname: '/movie', query: { id } }}
+                  as={`movie/${movie.id}`}
+                  href={{ pathname: '/movie', query: { id: movie.id } }}
                 >
                   <a className='link white'>
                     <img
-                      src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-                      alt={`Link to ${title}`}
+                      src={`https://image.tmdb.org/t/p/w500/${
+                        movie.poster_path
+                      }`}
+                      alt={`Link to ${movie.title}`}
                     />
-                    <h1 className='bold white'>{title}</h1>
+                    <h1 className='bold white'>{movie.title}</h1>
                   </a>
                 </Link>
                 <ul>
-                  {getGenres(genres, genre_ids).map((genre, i) => (
-                    <li key={i}>{genre}</li>
-                  ))}
+                  {getGenres(this.props.store.genres, movie.genre_ids).map(
+                    (genre, i) => <li key={i}>{genre}</li>
+                  )}
                 </ul>
               </div>
             ))}
           </div>
         )
       })
+    } else {
+      moviesList = <h1 className='text-center height100'>No movies found!!!</h1>
     }
     return (
       <div className='main'>
-        {requestStatus === 'pending' ? (
+        {this.props.store.requestStatus === 'pending' ? (
           <h1 className='text-center height100'>Loading...</h1>
         ) : (
           <div className='container padding--top25'>{moviesList}</div>
+        )}
+        {this.props.store.requestStatus === 'error' && (
+          <h1 className='text-center height1000' />
         )}
       </div>
     )
