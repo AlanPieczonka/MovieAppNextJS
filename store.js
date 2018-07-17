ed76117a6a86f0f53cc162c718e3bda809fa4416
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-17 19:19:09 +0200

    Refactor Mobx store and actions

--- store.js
+++ store.js
@@ -1,85 +1,57 @@
 import { observable, computed, action, autorun } from 'mobx'
 import { getAllMovies, searchMovie } from './utils/api'
+import { Throttle } from 'lodash-decorators'
 
-class SearchStore {
-  @observable searchTerm = ''
-
-  @action
-  setSearchTerm = searchTerm => {
-    this.searchTerm = searchTerm
-  }
-}
-
-class AppStore {
-  @observable searchStore
-  @observable requestStatus
+class Store {
   @observable movies = []
-  @observable initialMovies = []
+  @observable searchTerm = ''
+  @observable requestStatus = ''
 
-  constructor (searchStore) {
-    this.searchStore = searchStore
-    this.fetchInitialMovies()
-    autorun(
-      () => {
-        if (this.searchStore.searchTerm.length > 5) {
-          this.requestStatus = 'pending'
-          this.fetchSpecificMovies(this.searchStore.searchTerm)
-        } else if (
-          this.searchStore.searchTerm.length < 5 &&
-          this.initialMovies !== null
-        ) {
-          this.movies = this.initialMovies
-        }
-      },
-      { delay: 1000 }
-    )
+  constructor ({ movies, genres }) {
+    this.initialMovies = movies
+    this.movies = this.initialMovies
+    this.genres = genres
+    this.searchTerm = ''
+    this.requestStatus = 'done'
   }
 
   @action
-  fetchInitialMovies () {
+  setSearchTerm (term) {
+    this.searchTerm = term
+    this.searchTerm.length > 0
+      ? this.searchMovies()
+      : this.setMovies(this.initialMovies)
+  }
+
+  @Throttle(1500, { leading: false })
+  @action
+  async searchMovies () {
     this.requestStatus = 'pending'
-    getAllMovies().then(
-      action('fetchInitialMoviesSuccess', movies => {
-        this.initialMovies = movies
-        this.movies = this.initialMovies
-        this.requestStatus = 'done'
-      }),
-      action('fetchAllMoviesEror', error => {
-        this.requestStatus = 'error'
-      })
-    )
+
+    let movies
+    try {
+      movies = await searchMovie(this.searchTerm)
+      this.setMovies(movies)
+    } catch (err) {
+      this.requestStatus = 'error'
+    }
   }
 
   @action
-  fetchAllMovies () {
-    this.requestStatus = 'pending'
-    getAllMovies().then(
-      action('fetchAllMoviesSucess', movies => {
-        this.movies = movies
-        this.requestStatus = 'done'
-      }),
-      action('fetchAllMoviesError', error => {
-        this.requestStatus = 'error'
-      })
-    )
-  }
-
-  @action
-  fetchSpecificMovies (query) {
-    this.requestStatus = 'pending'
-    searchMovie(query).then(
-      action('fetchSingleMovieSuccess', movies => {
-        this.movies = movies
-        this.requestStatus = 'done'
-      }),
-      action('fetchSingleMovieError', error => {
-        this.requestStatus = 'error'
-      })
-    )
+  setMovies (movies) {
+    this.movies = movies
+    this.requestStatus = 'done'
   }
 }
 
-const searchStore = new SearchStore()
-const appStore = new AppStore(searchStore)
-
-export { appStore, searchStore }
+let store = null
+export function initializeStore (isServer) {
+  if (isServer) {
+    return new Store(isServer)
+  } else {
+    if (store === null) {
+      store = new Store(isServer)
+    }
+    return store
+  }
+}
