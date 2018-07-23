d42692c82123ddd4b719408fa4738f538b84fc6f
Alan Pieczonka <alanpieczonka@gmail.com>
2018-07-23 14:49:09 +0200

    Display spinner or image placeholder only if searchTerm is not blank

--- components/MovieCell.js
+++ components/MovieCell.js
@@ -2,7 +2,10 @@ import Img from 'react-image'
 import Link from 'next/link'
 import getGenres from '../utils/getGenres'
 import { RingLoader } from 'react-spinners'
+import { observer, inject } from 'mobx-react'
 
+@inject('store')
+@observer
 class MovieCell extends React.Component {
   render () {
     const { movie, genres } = this.props
@@ -14,21 +17,28 @@ class MovieCell extends React.Component {
           href={{ pathname: '/movie', query: { id: movie.id } }}
         >
           <a className='link white'>
-            <Img
-              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-              className='image-list'
-              loader={
-                <div className='flex-center'>
-                  <RingLoader color={'#123abc'} />
-                </div>
-              }
-              unloader={
-                <img
-                  src='https://i.pinimg.com/favicons/a4f459086724e56f18d3771b0bb2a184d22c3260a8f43d7d924a6722.png?95a08673b219455ac7272f1ce4a6b552'
-                  className='image-list'
-                />
-              }
-            />
+            {this.props.store.searchTerm === '' ? (
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                className='image-list'
+              />
+            ) : (
+              <Img
+                src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                className='image-list'
+                loader={
+                  <div className='flex-center'>
+                    <RingLoader color={'#123abc'} />
+                  </div>
+                }
+                unloader={
+                  <img
+                    src='https://i.pinimg.com/favicons/a4f459086724e56f18d3771b0bb2a184d22c3260a8f43d7d924a6722.png?95a08673b219455ac7272f1ce4a6b552'
+                    className='image-list'
+                  />
+                }
+              />
+            )}
             <h1 className='bold white'>{movie.title}</h1>
           </a>
         </Link>
